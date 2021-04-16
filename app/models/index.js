38f2142c7faf4e86bd9c2c4dc042637547c04657
01 <a01@192-168-1-6.tpgi.com.au>
2021-04-16 23:33:37 +1000

    point to aiven

--- app/models/index.js
+++ app/models/index.js
@@ -5,56 +5,56 @@ import users from './user.js';
 import sqltestusers from './sqltestuser.js';
 import sqltestgoogleusers from './sqltestgoogleusers.js';
 import results from './result.js';
-// const sslrootcert = './app/config/ca.pem';
-
-// const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
-//   host: config.HOST,
-//   port: config.PORT,
-//   dialect: config.DIALECT,
-//   native: true,
-//   ssl: true,
-//   dialectOptions: {
-//     ssl: {
-//       sslmode: 'verify-ca',
-//       sslrootcert,
-//     },
-//   },
-//   // operatorsAliases: {
-//   //   $and: Sequelize.and,
-//   //   $or: Sequelize.or,
-//   //   $eq: Sequelize.eq,
-//   //   $gt: Sequelize.gt,
-//   //   $lt: Sequelize.lt,
-//   //   $lte: Sequelize.lte,
-//   //   $like: Sequelize.like,
-//   // },
-//   // pool: {
-//   //   max: config.POOL.max,
-//   //   min: config.POOL.min,
-//   //   acquire: config.POOL.acquire,
-//   //   idle: config.POOL.idle,
-//   // },
-// });
+const sslrootcert = './app/config/ca.pem';
 
 const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
   host: config.HOST,
-  dialect: config.dialect,
-  operatorsAliases: {
-    $and: Sequelize.and,
-    $or: Sequelize.or,
-    $eq: Sequelize.eq,
-    $gt: Sequelize.gt,
-    $lt: Sequelize.lt,
-    $lte: Sequelize.lte,
-    $like: Sequelize.like,
+  port: config.PORT,
+  dialect: config.DIALECT,
+  native: true,
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      sslmode: 'verify-ca',
+      sslrootcert,
+    },
   },
-  pool: {
-    max: config.pool.max,
-    min: config.pool.min,
-    acquire: config.pool.acquire,
-    idle: config.pool.idle,
-  },
+  // operatorsAliases: {
+  //   $and: Sequelize.and,
+  //   $or: Sequelize.or,
+  //   $eq: Sequelize.eq,
+  //   $gt: Sequelize.gt,
+  //   $lt: Sequelize.lt,
+  //   $lte: Sequelize.lte,
+  //   $like: Sequelize.like,
+  // },
+  // pool: {
+  //   max: config.POOL.max,
+  //   min: config.POOL.min,
+  //   acquire: config.POOL.acquire,
+  //   idle: config.POOL.idle,
+  // },
 });
+
+// const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+//   host: config.HOST,
+//   dialect: config.dialect,
+//   operatorsAliases: {
+//     $and: Sequelize.and,
+//     $or: Sequelize.or,
+//     $eq: Sequelize.eq,
+//     $gt: Sequelize.gt,
+//     $lt: Sequelize.lt,
+//     $lte: Sequelize.lte,
+//     $like: Sequelize.like,
+//   },
+//   pool: {
+//     max: config.pool.max,
+//     min: config.pool.min,
+//     acquire: config.pool.acquire,
+//     idle: config.pool.idle,
+//   },
+// });
 const db = {};
 
 db.Sequelize = Sequelize;
